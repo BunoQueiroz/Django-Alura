6f786f4a946b6bdd4ee50f772e6fbfc82d323d6d
Bruno Castro <bs8872491@gmail.com>
2022-05-30 07:40:20 -0300

    Pegando todos os objetos do banco e permitindo-os serem exibidos no html de forma dinâmica

--- pratos/views.py
+++ pratos/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from .models import Pratos
 
 def index(request):
-    return render(request, 'index.html')
+    pratos = Pratos.objects.all()
 
-def pratos(request):
-    return render(request, 'pratos.html')
+    dados = {
+        'pratos': pratos
+    }
+
+    return render(request, 'index.html', dados)
+
+def pratos(request, pratos_id):
+    prato = get_object_or_404(Pratos, pk=pratos_id)
+    prato_a_exibir = {
+        'prato': prato
+    }
+    return render(request, 'pratos.html', prato_a_exibir)
