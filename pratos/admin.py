c17d0975358707ee57ec5aaeed54ea5b96fc6c21
Bruno Castro <bs8872491@gmail.com>
2022-06-07 09:19:15 -0300

    Melhorando a página Admin

--- pratos/admin.py
+++ pratos/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Pratos
 
-admin.site.register(Pratos)
+class ListaPratos(admin.ModelAdmin):
+    list_display = ('id', 'nome_prato', 'categoria')
+    list_display_links = ('id', 'nome_prato')
+    search_fields = ('nome_prato',)
+    list_filter = ('categoria',)
+    list_per_page = 5
+
+admin.site.register(Pratos, ListaPratos)
