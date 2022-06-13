3397eb227fae5094125210f710ec3bdd251c2c88
Bruno Castro <bs8872491@gmail.com>
2022-06-13 08:00:43 -0300

    Refatoração: mudando o nome das partials e melhorando o models

--- pratos/models.py
+++ pratos/models.py
@@ -13,3 +13,6 @@ class Prato(models.Model):
     publicada = models.BooleanField(default=False)
     data_criacao = models.DateTimeField(default=datetime.now())
     foto_prato = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
+
+    def __str__(self):
+        return self.nome_prato
