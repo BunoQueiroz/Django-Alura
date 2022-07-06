9055a94dc09bdec70c53f328baa594a145a48797
Bruno Castro <bs8872491@gmail.com>
2022-07-06 15:36:51 -0300

    Fazendo validação no cadastro e inserindo usuários no BD

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+def isEmpty(list_itens):
+    for i in list_itens:
+        if i.strip() == '':
+            return True
+        else:
+            return False
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        name = request.POST['nome']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        data = [name, email, password, password2]
+
+        if isEmpty(data) or password != password2 or User.objects.filter(email=email).exists():
+            return render(request, 'users/registration.html')
+        else:
+            user = User.objects.create(is_superuser=False, username=name, email=email, password=password)
+            user.save()
+            
+            print(data)
+            return redirect('login')
+    else:
+        return render(request, 'users/registration.html')
 
 def login(request):
     return render(request, 'users/login.html')
